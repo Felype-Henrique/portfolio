cded031b0597ec1cd1f68e089acf759fef780bf1
Felype Henrique <felypehenrique25@gmail.com>
2024-02-21 07:51:44 -0300

    Global CSS

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,5 +1,19 @@
 export default function HeroSection() {
     return (
-        <h1>HeroSection</h1>
+        <section id="heroSection" className="hero--section">
+            <div className="hero--section--content-box">
+                <div className="hero--section--content">
+                    <p className="section--title">Olá,Eu sou Felype</p>
+                    <h1 className="hero--section--title">
+                        <span className="hero--section--title--color">Desenvolverdor Full Stack</span>{""}
+                    </h1>
+                    <p className="hero--section--description">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        <br />Lorem ipsum dolor sit amet.
+                    </p>
+                </div>
+                <button className="btn btn-primary"></button>
+            </div>
+        </section>
     )
 }
